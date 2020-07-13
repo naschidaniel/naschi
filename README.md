35b38f5acf844e0d58f25686aa22d7ebd0afa534
Daniel Naschberger <daniel@naschi.at>
2020-07-13 22:04:33 +0200

    init

---
+++ README.md
@@ -0,0 +1,26 @@
+# naschi.at
+
+This is my personal website. naschidaniel.github.io
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <h1>naschi.at</h1>
+    <p>This domain is only used for email redirect.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  margin: 100px 0 0;
+  color: #d3d3d3;
+}
+p {
+  color: #d3d3d3;
+  font-size: 150%;
+}
+</style>
