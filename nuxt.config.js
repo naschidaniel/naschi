0c25ba1878d76950e0a1f9e36e61d5f5bf9e24f8
Daniel Naschberger <daniel@naschi.at>
2020-07-14 20:21:21 +0200

    add logo

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,6 +18,9 @@ export default {
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { name: "msapplication-TileColor", content: "#ffffff" },
+      { name: "msapplication-TileImage", content: "/ms-icon-144x144.png" },
+      { name: "theme-color", content: "#ffffff" },
       {
         hid: "description",
         name: "description",
@@ -28,14 +31,54 @@ export default {
     link: [
       {
         rel: "apple-touch-icon",
+        sizes: "57x57",
+        href: "/apple-icon-57x57.png",
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "60x60",
+        href: "/apple-icon-60x60.png",
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "72x72",
+        href: "/apple-icon-72x72.png",
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "76x76",
+        href: "/apple-icon-76x76.png",
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "114x114",
+        href: "/apple-icon-114x114.png",
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "120x120",
+        href: "/apple-icon-120x120.png",
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "144x144",
+        href: "/apple-icon-144x144.png",
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "152x152",
+        href: "/apple-icon-152x152.png",
+      },
+      {
+        rel: "apple-touch-icon",
         sizes: "180x180",
-        href: "/apple-touch-icon.png",
+        href: "/apple-icon-180x180.png",
       },
       {
         rel: "icon",
         type: "image/png",
-        sizes: "16x16",
-        href: "/favicon-16x16.png",
+        sizes: "192x192",
+        href: "/android-icon-192x192.png",
       },
       {
         rel: "icon",
@@ -43,7 +86,19 @@ export default {
         sizes: "32x32",
         href: "/favicon-32x32.png",
       },
-      { rel: "manifest", href: "/site.webmanifest" },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "96x96",
+        href: "/favicon-96x96.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicon-16x16.png",
+      },
+      { rel: "manifest", href: "/manifest.json" },
       {
         rel: "stylesheet",
         href:
