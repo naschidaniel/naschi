e95b437b8d4340349d0c8f6af7839fdf40796c72
Daniel Naschberger <daniel@naschi.at>
2021-05-06 21:32:28 +0200

    fix(TheFooter): prettier

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -4,17 +4,13 @@
       &#169; Daniel Naschberger
     </small>
     <nuxt-link to="/DSVGO">
-      <small class="text-sm text-blue-500 mr-6">
-        DSVGO
-      </small>
+      <small class="text-sm text-blue-500 mr-6"> DSVGO </small>
     </nuxt-link>
     <nuxt-link to="/impressum">
-      <small class="text-sm text-blue-500 mr-6">
-        Impressum
-      </small>
+      <small class="text-sm text-blue-500 mr-6"> Impressum </small>
     </nuxt-link>
     <small class="text-sm text-blue-500 mr-6">
-      Systeminfo: {{ NUXT_ENV_CURRENT_GIT_SHA }}
+      Version: {{ NUXT_ENV_CURRENT_GIT_SHA }}
     </small>
   </div>
 </template>
