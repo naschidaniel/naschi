9142ed8e377d65792943705e857b6c85f7b61ee6
Daniel Naschberger <daniel@naschi.at>
2021-12-05 16:53:49 +0100

    fix(eslint): delete duplicate

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,15 +9,13 @@ module.exports = {
     "eslint:recommended",
     "@vue/typescript",
     "@vue/prettier",
-    "@vue/prettier/@typescript-eslint"
+    "@vue/prettier/@typescript-eslint",
   ],
-  parserOptions: {
-    ecmaVersion: 2020,
-  },
   parser: "vue-eslint-parser",
   parserOptions: {
+    ecmaVersion: 2020,
     parser: "@typescript-eslint/parser",
-    sourceType: "module"
+    sourceType: "module",
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
